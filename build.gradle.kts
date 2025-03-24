54c0230661d61c9cce37de66d1ea12d34c41df98
unknown <iamahad1570@gmail.com>
2025-03-24 13:32:18 +0600

    24/03/2025 Sync Error Solved

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,3 @@ plugins {
     id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
